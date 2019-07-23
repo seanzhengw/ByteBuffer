f8e3369e51cb807cd943462b75e2605cf5222f40
Sean Zheng <seanzhengw@gmail.com>
2019-07-23 11:29:33 +0800

    implement ByteBuffer

--- src/ByteBuffer/ByteBuffer.h
+++ src/ByteBuffer/ByteBuffer.h
@@ -3,6 +3,7 @@
 
 #include "ReadableByteBuffer.h"
 #include "WritableByteBuffer.h"
+#include <string.h>
 
 // ByteBuffer is a class for byte array operating,
 // use array with stream-like read/write interface.
@@ -65,5 +66,160 @@ private:
     // Buffer
     uint8_t *mBuf;
 };
+
+inline ByteBuffer::ByteBuffer(size_t size) : mSize(size), mWrites(0), mReads(0), mBuf(new uint8_t[size])
+{
+}
+
+inline ByteBuffer::ByteBuffer(const ByteBuffer &b) : mSize(b.mSize), mWrites(b.mWrites), mReads(b.mReads), mBuf(new uint8_t[b.mSize])
+{
+    memcpy(mBuf, b.mBuf, mWrites);
+}
+
+inline ByteBuffer &ByteBuffer::operator=(const ByteBuffer &b)
+{
+    delete[] mBuf;
+    mSize = b.mSize;
+    mWrites = b.mWrites;
+    mReads = b.mReads;
+    mBuf = new uint8_t[b.mSize];
+    memcpy(mBuf, b.mBuf, mWrites);
+    return (*this);
+}
+
+inline ByteBuffer::~ByteBuffer()
+{
+    delete[] mBuf;
+}
+
+inline void ByteBuffer::Resize(size_t size)
+{
+    uint8_t *buf = mBuf;
+    mBuf = new uint8_t[size];
+    mSize = size;
+    mWrites = mWrites > mSize ? mSize : mWrites;
+    mReads = mReads > mSize ? mSize : mReads;
+    memcpy(mBuf, buf, mSize);
+    delete[] buf;
+}
+
+inline void ByteBuffer::Rewind()
+{
+    mReads = 0;
+}
+
+inline void ByteBuffer::Compact()
+{
+    if (mReads == 0)
+    {
+        return;
+    }
+    size_t size = Remaining();
+    memmove(mBuf, mBuf + mReads, size);
+    mReads = 0;
+    mWrites = size;
+}
+
+inline void ByteBuffer::Reset()
+{
+    mReads = 0;
+    mWrites = 0;
+}
+
+inline size_t ByteBuffer::GetWritePos()
+{
+    return mWrites;
+}
+
+inline size_t ByteBuffer::GetReadPos()
+{
+    return mReads;
+}
+
+inline size_t ByteBuffer::GetSize()
+{
+    return mSize;
+}
+
+inline size_t ByteBuffer::Writable()
+{
+    return mSize - mWrites;
+}
+
+inline void ByteBuffer::Write(uint8_t b)
+{
+    if (mWrites < mSize)
+    {
+        mBuf[mWrites] = b;
+        mWrites++;
+    }
+}
+
+inline void ByteBuffer::Write(const uint8_t *bytes, size_t size)
+{
+    if (Writable() >= size)
+    {
+        memcpy(&mBuf[mWrites], bytes, size);
+        mWrites += size;
+    }
+}
+
+inline void ByteBuffer::Write(const char *str)
+{
+    Write((uint8_t *)str, strlen(str));
+}
+
+inline void ByteBuffer::Write(const char *str, size_t size)
+{
+    Write((const uint8_t *)str, size);
+}
+
+inline size_t ByteBuffer::Remaining()
+{
+    return mWrites - mReads;
+}
+
+inline uint8_t ByteBuffer::Read()
+{
+    if (mWrites > mReads)
+    {
+        return mBuf[mReads++];
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+inline void ByteBuffer::Read(void *buf, size_t size)
+{
+    size_t readsize = Remaining() >= size ? size : Remaining();
+    memcpy(buf, &mBuf[mReads], readsize);
+    mReads += readsize;
+}
+
+inline const char *ByteBuffer::CString()
+{
+    if (mWrites < mSize)
+    {
+        mBuf[mWrites] = 0;
+    }
+    return (const char *)(&mBuf[mReads]);
+}
+
+inline uint8_t *ByteBuffer::GetBuf()
+{
+    return mBuf;
+}
+
+inline uint8_t &ByteBuffer::operator[](unsigned int i)
+{
+    return mBuf[i];
+}
+
+inline uint8_t ByteBuffer::operator[](unsigned int i) const
+{
+    return mBuf[i];
+}
 
 #endif
